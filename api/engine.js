bfa7e390b86f586330d7e4aa3852cf22e2be0593
Your Name <your@email.com>
2026-02-27 19:21:11 +0530

    Test allocation import

--- api/engine.js
+++ api/engine.js
@@ -1,13 +1,15 @@
 import { evaluateLifecycle } from "../lib/lifecycle.js";
 import { calculateCalories } from "../lib/calories.js";
 import { calculateMacros } from "../lib/macro.js";
+import { allocateIngredients } from "../lib/allocation.js";
 
 export default async function handler(req, res) {
   try {
     return res.status(200).json({
       lifecycle_type: typeof evaluateLifecycle,
       calories_type: typeof calculateCalories,
-      macro_type: typeof calculateMacros
+      macro_type: typeof calculateMacros,
+      allocation_type: typeof allocateIngredients
     });
   } catch (err) {
     return res.status(500).json({
