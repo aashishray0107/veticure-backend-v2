d5884c38c1068495b12d27fe0ca8825e80b02d9c
Your Name <your@email.com>
2026-02-27 19:18:19 +0530

    Test lifecycle + calories import

--- api/engine.js
+++ api/engine.js
@@ -1,9 +1,11 @@
 import { evaluateLifecycle } from "../lib/lifecycle.js";
+import { calculateCalories } from "../lib/calories.js";
 
 export default async function handler(req, res) {
   try {
     return res.status(200).json({
-      lifecycle_type: typeof evaluateLifecycle
+      lifecycle_type: typeof evaluateLifecycle,
+      calories_type: typeof calculateCalories
     });
   } catch (err) {
     return res.status(500).json({
