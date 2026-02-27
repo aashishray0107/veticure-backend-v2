e64356cb0e44207bf834b093557da0e60c825887
Your Name <your@email.com>
2026-02-27 19:20:16 +0530

    Test macro import

--- api/engine.js
+++ api/engine.js
@@ -1,11 +1,13 @@
 import { evaluateLifecycle } from "../lib/lifecycle.js";
 import { calculateCalories } from "../lib/calories.js";
+import { calculateMacros } from "../lib/macro.js";
 
 export default async function handler(req, res) {
   try {
     return res.status(200).json({
       lifecycle_type: typeof evaluateLifecycle,
-      calories_type: typeof calculateCalories
+      calories_type: typeof calculateCalories,
+      macro_type: typeof calculateMacros
     });
   } catch (err) {
     return res.status(500).json({
